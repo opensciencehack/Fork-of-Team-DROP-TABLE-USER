5303283c54e8aee89e55e46a542d71c8998b8601
Bumpfel <ee222yb@student.lnu.se>
2019-04-07 02:58:08 +0200

    cluster graph tweets w/o stopwords

---
+++ src/GraphStream.java
@@ -0,0 +1,82 @@
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Scanner;
+
+import com.fasterxml.jackson.databind.JsonNode;
+
+import org.graphstream.algorithm.TarjanStronglyConnectedComponents;
+import org.graphstream.graph.Graph;
+import org.graphstream.graph.Node;
+import org.graphstream.graph.implementations.MultiGraph;
+
+public class GraphStream {
+
+	public static void main(String[] args) throws IOException {
+
+		GraphStream gs = new GraphStream();
+		File file = new File("TweetData/tweetsWithoutStopwords_partial.txt");
+		
+		ArrayList<String> tweets = new JsonParser().parseTextFile(file);
+
+		gs.drawGraph(tweets);
+	}
+
+	public void drawGraph(ArrayList<String> tweets) {		
+		Graph graph = new MultiGraph("g");
+		
+		// String[] tweets = { };
+		// "Min systers pojkv�n �r diabetiker, och n�r han tog insulin sk�mtade jag om att det var heroin. Min syster blev j�ttearg o�",
+		// "Tack alla ssk som kom till @ssdf12diabetes utbildningskv�ll om diabetes. Ni �r v�ra vardagshj�ltar! #diabetesdebatt",
+		// "Tack Linda Kjerr och g�nget fr�n  \"Akademiskt specialistcentrum f�r diabetes\" f�r en inspirerande beskrivning av er�",
+		// "Usch, tungt! :( Att tvingas t�nka om �r jobbigt. Men for what its worth, det beh�ver inte vara s� dr�",
+		// "Typ 1-diabetes: Flera f�rdelar med tidig diagnos",
+		// "\"Fr�n v�rd p� piedestal till v�rd med delaktighet.\", s�ger @aknergardh Viktigt och sj�lvklart tycker vi p�",
+		// "H�lso- och sjukv�rdspersonal kan hj�lpa sina patienter med #diabetes som v�ljer att fasta genom att dela denna video https:�",
+		// "Vet du hur du ska r�da dina patienter med diabetes som v�ljer att fasta? Klicka p� l�nken nedan f�r att dela med en� ",
+		// "Det finns intressanta f�rslag i delbet�nkandet som @aknergardh l�mna idag. Vi vill specifikt lyfta fram f�rslagen om sy�",
+		// "Prim�rv�rden navet i en personcentrerad v�rd - om @aknergardh delbet�nkande",
+		// "L�ngsiktig viktuppg�ng kan i vissa fall f�rklaras av nedsatt f�rm�ga att oms�tta fett, visar en ny studie fr�n Karo",
+		// "N�r l�karen en tr�ffat en g�ng tidigare talar till en som om man haft diabetes i mindre �n ett halv�r, fast man har",
+		// "Prim�rv�rden navet i en personcentrerad v�rd - om @aknergardh delbet�nkande",
+		// "Det finns intressanta f�rslag i delbet�nkandet som @aknergardh l�mna idag. Vi vill specifikt lyfta fram f�rslagen om sy",
+		// "Pa Dibba ger pengar till Barndiabetesfonden" };
+		
+		ArrayList<String> allWords = new ArrayList<>();
+		ArrayList<Node> tweetNodes = new ArrayList<>();
+		
+		for(int tweetId = 0; tweetId < tweets.size(); tweetId ++) {
+			ArrayList<String> uniqueTweetWords = new ArrayList<>();
+			
+			Node tweetNode  = graph.addNode("tId" + tweetId);
+			tweetNodes.add(tweetNode);
+			tweetNode.addAttribute("ui.style", "shape:circle;fill-color: red;size: 5px;");
+			// tweetNode.addAttribute("ui.label", tweetId);
+			String[] words = tweets.get(tweetId).split(" ");
+			for(String word : words) {
+				if(!allWords.contains(word)) {
+					allWords.add(word);
+					Node wordNode = graph.addNode(word);
+					wordNode.addAttribute("ui.style", "shape:circle;fill-color: blue;size: 5px;");
+					// wordNode.addAttribute("ui.label", word);
+				}
+				if(!uniqueTweetWords.contains(word)) {
+					uniqueTweetWords.add(word);
+					if(graph.getEdge("tId" + tweetId + word) == null) {
+						graph.addEdge("tId" + tweetId + word, "tId" + tweetId, word);
+					}
+				}
+			}
+		}
+
+		TarjanStronglyConnectedComponents tscc = new TarjanStronglyConnectedComponents();
+		tscc.init(graph);
+		tscc.compute();
+	
+		graph.display();
+	}
+}
--- src/JsonParser.java
+++ src/JsonParser.java
@@ -1,7 +1,10 @@
 import java.io.File;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Scanner;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -10,23 +13,50 @@ public class JsonParser {
 
 	private final String DATA_FOLDER = "TweetData/";
 	
-	// public static void main(String[] args) throws IOException {
-	// 	JsonParser jp = new JsonParser();
-	// 	ArrayList<JsonNode> uniqueTweets = jp.parseUniqueTweets();
-	// 	jp.writeJson(uniqueTweets, "outputFile");
-	// }
+	 public static void main(String[] args) throws IOException {
+	 	JsonParser jp = new JsonParser();
+	 	ArrayList<JsonNode> uniqueTweets = jp.parseJson("unique_tweets");
+
+	 	jp.writeTweetsWithoutStopWords(uniqueTweets);
+	}
     
-    public ArrayList<JsonNode> parseJson() throws IOException {
-        File dataFile = new File(DATA_FOLDER + "diabetes_tweets-array.json");
+    public ArrayList<JsonNode> parseJson(String file) throws IOException {
+        File dataFile = new File(DATA_FOLDER + file + ".json");
         
         ObjectMapper oMapper = new ObjectMapper();
         JsonNode[] j = oMapper.readValue(dataFile, JsonNode[].class);
         
         return new ArrayList<JsonNode>(Arrays.asList(j));
-    }
+	}
+	
+	/**
+	 * Writes a text file filtering out stop words from the tweets (used for node graph)
+	 * @param tweets
+	 * @throws IOException
+	 */
+	private void writeTweetsWithoutStopWords(ArrayList<JsonNode> tweets) throws IOException {
+		PrintWriter pw = new PrintWriter(DATA_FOLDER + "/tweetsWithoutStopwords.txt");
+		
+		ArrayList<String> stopwords = parseTextFile(new File("stopwords-sv.txt"));
+		for(JsonNode j : tweets) {
+			String tweet = j.get("text").toString();
+			tweet = tweet.replace("@", "");
+			tweet = tweet.replace("#", "");
+			tweet = tweet.replace("\"", "");
+			tweet = tweet.toLowerCase();
+
+			for(String stopword : stopwords) {
+				if(tweet.indexOf(stopword) == 0)
+					tweet = tweet.replaceAll(stopword + " ", " ");
+				tweet = tweet.replaceAll(" " + stopword + " ", " ");
+			}
+			pw.println(tweet);
+		}
+		pw.close();
+	}
 
 	private ArrayList<JsonNode> parseUniqueTweets() throws IOException {
-		ArrayList<JsonNode> allTweets = parseJson();
+		ArrayList<JsonNode> allTweets = parseJson("diabetes_tweets-array");
 		ArrayList<JsonNode> allUniqueTweets = new ArrayList<>();
 		
 		for(JsonNode j : allTweets) {
@@ -51,5 +81,18 @@ public class JsonParser {
 		catch (Exception e) {
 			e.printStackTrace();
 		}
+	}
+
+	public ArrayList<String> parseTextFile(File file) throws IOException {
+		Scanner sc = new Scanner(file);
+		
+		ArrayList<String> list = new ArrayList<>();
+		
+		while(sc.hasNextLine()) {
+			list.add(sc.nextLine());
+		}
+		sc.close();
+
+		return list;
 	}
 }
